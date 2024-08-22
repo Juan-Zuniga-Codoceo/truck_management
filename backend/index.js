f141a297d4a5b79c6256cb3ab73c3e0ad7902e83
Juan-Zuniga-Codoceo <zcja.89@gmail.com>
2024-08-22 18:26:34 -0400

    Solucionado problema servidor

--- backend/index.js
+++ backend/index.js
@@ -1,15 +1,25 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const driverRoutes = require('./routes/driverRoutes');
-const routeRoutes = require('./routes/routeRoutes'); // Importar las rutas de rutas
+const routeRoutes = require('./routes/routeRoutes');
 const app = express();
 
 // Middleware
 app.use(bodyParser.json()); // O usa express.json() si estás en Express 4.16+
 
+// Ruta raíz
+app.get('/', (req, res) => {
+  res.send('Bienvenido a la API de gestión de camiones');
+});
+
 // Rutas
-app.use('/api/drivers', driverRoutes); // Rutas para los controladores de drivers
-app.use('/api/routes', routeRoutes); // Rutas para los controladores de rutas
+app.use('/api/drivers', driverRoutes);
+app.use('/api/routes', routeRoutes);
+
+// Manejador de 404 para rutas no encontradas
+app.use((req, res, next) => {
+  res.status(404).send('Lo siento, no se pudo encontrar esa ruta');
+});
 
 // Iniciar el servidor
 const PORT = process.env.PORT || 3000;
